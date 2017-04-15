3be440584481d7da13b30052f4e7baaad51456b8
Slađan Simic <SIM@SIMs-MacBook-Pro.local>
2017-04-15 21:55:29 +0200

    remove signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+
+         #zbrisan :registerable -> ni mozna registracija novih userjev na pageu
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   get "welcome/index"
   root "welcome#index"
 
+  get "*path" => redirect("/")
+
 end
