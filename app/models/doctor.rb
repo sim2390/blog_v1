0477f86471570684ea9157f7e567b903fafc9aea
Slađan Simic <SIM@SIMs-MacBook-Pro.local>
2017-04-20 20:42:41 +0200

    heroku new doctor error

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -4,6 +4,8 @@ class Doctor < ApplicationRecord
   friendly_id :name, use: :slugged
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  validates_attachment_content_type :avatar, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+
+
 
 end
